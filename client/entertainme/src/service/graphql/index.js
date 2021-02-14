7edf84f39a6993cdfc5bfcc55698b01c1bf47e5d
efrizal-m <efrizalsy0424@gmail.com>
2021-02-14 17:38:02 +0700

    add ec2 public ip-test

--- client/entertainme/src/service/graphql/index.js
+++ client/entertainme/src/service/graphql/index.js
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { favoriteMoviesVar } from '../../cache'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  // uri: 'http://localhost:4000',
+  uri: 'http://13.250.22.237:4000',
   cache: new InMemoryCache({
     typePolicies: {
       Query : {
--- server/orchestrator/config/axiosinstance_movies.js
+++ server/orchestrator/config/axiosinstance_movies.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4001',
-  // baseURL: 'http://13.250.22.237:4001',
+  // baseURL: 'http://localhost:4001',
+  baseURL: 'http://13.250.22.237:4001',
 });
 
 export default instance;
